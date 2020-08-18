1e2880a2172204c6b9d2abbd144fca2d1b0bae4b
ie <ie@includer.com>
2020-08-18 18:01:00 +0800

    配置了系统文件

--- book_manage/book_manage/settings/demo.py
+++ book_manage/book_manage/settings/demo.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+path = sys.path
+path.append(os.path.join(BASE_DIR, 'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'books'
+
 ]
 
 MIDDLEWARE = [
@@ -73,13 +76,27 @@ WSGI_APPLICATION = 'book_manage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.ysql',
+#         'HOST':' 127.0.0.1',
+#         'PORT': 3306,
+#         'USER': 'root',
+#         'PASSWORD': '123123',
+#         'NAME': 'book_magage',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        "HOST": "127.0.0.1",
+        'PORT': 3306,
+        "USER": "root",
+        "PASSWORD": "123123",
+        'NAME': "book_magage",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
