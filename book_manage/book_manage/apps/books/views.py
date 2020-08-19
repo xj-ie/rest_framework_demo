ff7184089d06b41f1ca585ea319aa13c89693a74
ie <ie@includer.com>
2020-08-20 07:56:00 +0800

    rest_APIView的使用

--- book_manage/book_manage/apps/books/views.py
+++ book_manage/book_manage/apps/books/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
-
+# from requests import Response
+from rest_framework.views import APIView, Response
 from books.models import BookInfo
 
 from books.serializers import Bookserializer
@@ -14,15 +15,17 @@ from books.serializers import Bookserializer
 from books.serializers import BookValserializer
 
 
-class Books(View):
+class Books(APIView):
     def get(self,request):
+        print(request.query_params) #=>djaongo.views.View request.GET
         book = BookInfo.objects.all()
-        res = Bookserializer(book,many=True)
-        return http.JsonResponse(res.data, safe=False)
+        res = Bookserializer(book, many=True)
+        return Response(res.data)
     def post(self, request):
-        body = request.body.decode()
-        dict_str = json.loads(body)
-        res = BookValserializer(data=dict_str)
+        # body = request.body.decode()
+        # dict_str = json.loads(body)
+        data = request.data # 相等于上面两行
+        res = BookValserializer(data=data)
         res.is_valid()
         res.save()
-        return http.JsonResponse(res.data)
+        return Response(res.data)
