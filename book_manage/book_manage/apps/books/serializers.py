824deb3f05fc16d6c4324f39fe18879e98ae15af
ie <ie@includer.com>
2020-08-19 12:24:05 +0800

    rest序列嵌套使用

--- book_manage/book_manage/apps/books/serializers.py
+++ book_manage/book_manage/apps/books/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
+class PeopleInfoSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    description = serializers.CharField()
+
+
 
 class Bookserializer(serializers.Serializer):
     name = serializers.CharField()
     pub_date = serializers.DateField()
     readcount = serializers.IntegerField()
+    # peopleinfo_set = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    # peopleinfo_set = serializers.StringRelatedField(read_only=True, many=True)
+    peopleinfo_set = PeopleInfoSerializer(many=True)
+
