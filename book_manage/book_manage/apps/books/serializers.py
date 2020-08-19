d3dbbe444746724ef39b8d479e1d1acf308e8c66
ie <ie@includer.com>
2020-08-19 14:25:57 +0800

    自定义vildate

--- book_manage/book_manage/apps/books/serializers.py
+++ book_manage/book_manage/apps/books/serializers.py
@@ -17,5 +17,15 @@ class BookValserializer(serializers.Serializer):
     name = serializers.CharField(max_length=20,min_length=5)
     pub_date = serializers.DateField(required=False)
     readcount = serializers.IntegerField(max_value=100,min_value=5)
+    commentcount = serializers.IntegerField(default=10)
+    def validate_name(self,book_name):
+        if book_name == 'python':
+            raise serializers.ValidationError('书名不能是python')
+        return book_name
+    def validate(self,value):
+        if value['readcount']>value['commentcount']:
+            raise serializers.ValidationError('not readcount > commentcount')
+        return value
+
 
 
