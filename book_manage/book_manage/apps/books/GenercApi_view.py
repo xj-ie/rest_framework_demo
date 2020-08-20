7e1727e93f44d7878e60a727c566269319e5b22c
ie <ie@includer.com>
2020-08-20 17:44:57 +0800

    rest—view拓展类的使用与视图集的说明　

--- book_manage/book_manage/apps/books/GenercApi_view.py
+++ book_manage/book_manage/apps/books/GenercApi_view.py
@@ -11,22 +11,24 @@ from django.views import View
 from books.models import BookInfo
 from rest_framework.generics import GenericAPIView
 from rest_framework.views import  Response
-from rest_framework.mixins import ListModelMixin,RetrieveModelMixin,CreateModelMixin,DestroyModelMixin,UpdateModelMixin
+
 from books.serializers import Bookserializer
 
 from books.serializers import BookValserializer
-# import sys
-# sys.setrecursionlimit(100000)
 
 
-class Books(GenericAPIView, ListModelMixin, CreateModelMixin):
+class Books(GenericAPIView):
     queryset = BookInfo.objects.all()
     # ＃指定查询类视图数据
     # salizer_class = Bookserializer
     serializer_class = Bookserializer
 
     def get(self,request):
-        return self.list(request)
+
+        # print(request.query_params) #=>djaongo.views.View request.GET
+        book = self.get_queryset()  # 获取所有数据..      # self.get_objects()
+        res = self.get_serializer(book, many=True)
+        return Response(res.data)
     def post(self, request):
         # body = request.body.decode()
         # dict_str = json.loads(body)
@@ -35,10 +37,16 @@ class Books(GenericAPIView, ListModelMixin, CreateModelMixin):
         res.is_valid()
         res.save()
         return Response(res.data)
-class Books_update(GenericAPIView, UpdateModelMixin, DestroyModelMixin):
+class Books_update(GenericAPIView):
     queryset = BookInfo.objects.all()
     serializer_class = BookValserializer
     def put(self, request, pk):
-        return self.update(request, pk)
-    def delete(self, request, pk):
-        return self.delete(request, pk)
+        data = request.data
+        try:
+            book = self.get_object()
+        except Exception:
+            return http.HttpResponseForbidden('NON')
+        ser = self.get_serializer(book,data=data)
+        ser.is_valid()
+        ser.save()
+        return Response(ser.data)
