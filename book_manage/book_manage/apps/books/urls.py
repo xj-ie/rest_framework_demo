aa529268bfb0fe61dd544351073c8e98ff7a4f12
ie <273817691@qq.com>
2020-08-22 21:22:29 +0800

    路由的自定义方法

--- book_manage/book_manage/apps/books/urls.py
+++ book_manage/book_manage/apps/books/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url, include
-# from django.contrib import admin
+from django.conf.urls import url
 from . import views
+from rest_framework.routers import SimpleRouter
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     # url(r'^', inclu('books.urls'))
-    url(r'^book_drf$', views.BooksDRF.as_view({'get': 'list', 'post': 'create'})),
-    url(r'^book_drf/(?P<pk>\d+)$', views.BooksDRF.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}))
+    # url(r'^book_drf$', views.BooksDRF.as_view({'get': 'list', 'post': 'create'})),
+    # url(r'^book_drf/(?P<pk>\d+)$', views.BooksDRF.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}))
 ]
+router = SimpleRouter()
+router.register('book_drf', views.BooksDRF, basename='books_drf')
+print(router.urls)
+urlpatterns += router.urls
