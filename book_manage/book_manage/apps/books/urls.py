deae1a1d402dd978e0615f600a656854d94c14d8
ie <273817691@qq.com>
2020-08-22 20:58:13 +0800

    ModelViewSet的使用

--- book_manage/book_manage/apps/books/urls.py
+++ book_manage/book_manage/apps/books/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     # url(r'^', inclu('books.urls'))
     url(r'^book_drf$', views.BooksDRF.as_view({'get': 'list', 'post': 'create'})),
-    url(r'^book_drf/(?P<pk>\d+)$', views.BooksDRF.as_view({'put': 'update'}))
+    url(r'^book_drf/(?P<pk>\d+)$', views.BooksDRF.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}))
 ]
