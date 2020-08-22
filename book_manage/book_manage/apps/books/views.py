84ea0623800a61e5bb349796e3ecee8a8b1addcd
ie <273817691@qq.com>
2020-08-22 21:53:38 +0800

    action装饰器自动生成路由

--- book_manage/book_manage/apps/books/views.py
+++ book_manage/book_manage/apps/books/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from books.models import BookInfo
@@ -7,3 +8,8 @@ from books.serializers import BookValserializer
 class BooksDRF(ModelViewSet):
     queryset = BookInfo.objects.all()
     serializer_class = BookValserializer
+    @action(methods=['get'], detail=True)
+    def lastdata(self, request, pk):
+        book = BookInfo.objects.get(id=pk)
+        ser = BookValserializer(book)
+        return Response(ser.data)
