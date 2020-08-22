a4516def48c8c25c9ee13db3ac6794bcf524bd7f
ie <273817691@qq.com>
2020-08-23 07:37:47 +0800

    GenericApiView指定多个序列化器

--- book_manage/book_manage/apps/books/views.py
+++ book_manage/book_manage/apps/books/views.py
@@ -1,13 +1,23 @@
+from django.conf import settings
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from books.models import BookInfo
 from books.serializers import BookValserializer
+from books.serializers import Bookserializer
 
 
 class BooksDRF(ModelViewSet):
     queryset = BookInfo.objects.all()
-    serializer_class = BookValserializer
+
+    def get_serializer_class(self):  #可以通过 这个来指定多个序列 化器
+        if self.action in settings.Serializerchange_url.values(): #通过这个来区别 类型来指定序列化器
+            return BookValserializer
+        else:
+            return Bookserializer
+
+
+
     @action(methods=['get'], detail=True)
     def lastdata(self, request, pk):
         book = BookInfo.objects.get(id=pk)
--- book_manage/book_manage/settings/demo.py
+++ book_manage/book_manage/settings/demo.py
@@ -135,3 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+Serializerchange_url = {'put': 'update', 'get': 'lastdata', 'delete': 'deltet'}
+Serializer_url = {'put': 'create', 'get': 'list'}
